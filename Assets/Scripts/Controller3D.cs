75c620ddd12a8c9d020c0e7a663b22fce31c73f1
Daisykm <56872925+Daisykm@users.noreply.github.com>
2019-11-22 11:05:09 +0100

    player control updates

--- Assets/Scripts/Controller3D.cs
+++ Assets/Scripts/Controller3D.cs
@@ -42,7 +42,7 @@ public class Controller3D : MonoBehaviour
 
         rb.AddForce(new Vector3(moveHorizontal, 0.0f, moveVertical) * speed);
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
         {
             rb.AddForce(new Vector3(0, 5f, 0) * jumpSpeed, ForceMode.Impulse);
             isGrounded = false;
