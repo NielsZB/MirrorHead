536925fc7d5c45baa39c4e2684bf4d5f19ac78cd
Daisykm <56872925+Daisykm@users.noreply.github.com>
2019-11-24 23:10:06 +0100

    world offset

---
+++ Assets/WorldOffsetApply.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldOffsetApply : MonoBehaviour
+{
+    public Vector3 worldOffset;
+
+    public void LateUpdate()
+    {
+        transform.position = transform.parent.position + worldOffset;
+    }
+}
+
