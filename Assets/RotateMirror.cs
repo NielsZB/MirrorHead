42b7b3ff80b6556ead209bae7719a4aec00bae60
Daisykm <56872925+Daisykm@users.noreply.github.com>
2019-11-25 16:44:39 +0100

    player control changes

--- Assets/RotateMirror.cs
+++ Assets/RotateMirror.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RotateMirror : MonoBehaviour
 {
     public float mirrorSpeed = 2f;
-
+    public string mirrorButton = "Mirror_P1";
     void Start()
     {
        // print(transform.localEulerAngles.x);
@@ -14,7 +14,7 @@ public class RotateMirror : MonoBehaviour
 
         void Update()
     {
-        float mirrorRotate = Input.GetAxis("MouseX");
+        float mirrorRotate = Input.GetAxis(mirrorButton);
 
         transform.RotateAround(transform.position, (Vector3.forward * mirrorRotate * mirrorSpeed), Time.deltaTime * 90f);
         
