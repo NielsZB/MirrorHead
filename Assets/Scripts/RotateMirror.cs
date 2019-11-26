4e6fa438c00c13446f03dfb5359d945af4c551c1
Niels <nzhansen@gmail.com>
2019-11-26 22:12:40 +0100

    fefefe

--- Assets/Scripts/RotateMirror.cs
+++ Assets/Scripts/RotateMirror.cs
@@ -16,11 +16,12 @@ public class RotateMirror : MonoBehaviour
         x = Input.GetAxis(mirrorRotateX);
         y = Input.GetAxis(mirrorRotateY);
 
-        if (x != 0 && y != 0)
+        if (x != 0 || y != 0)
         {
             Quaternion q = Quaternion.Euler(0, 0, Mathf.Atan2(y, x) * Mathf.Rad2Deg);
             transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * mirrorSpeed);
         }
+
     }
 
     // Transform.localEulerAngles = new Vector3(0, Mathf.Atan2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * 180 / Mathf.PI, 0);
